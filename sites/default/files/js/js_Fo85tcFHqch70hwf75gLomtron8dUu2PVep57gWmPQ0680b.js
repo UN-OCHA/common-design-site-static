f4e2b89bd76ceab489e212c062f07431e7accfe2
joempora <joempora@gmail.com>
2024-12-11 08:04:31 +0100

    initial upload
    static copy taken 11 Dec 2024

---
+++ sites/default/files/js/js_Fo85tcFHqch70hwf75gLomtron8dUu2PVep57gWmPQ0680b.js
@@ -0,0 +1,2 @@
+/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+(function(Drupal){'use strict';Drupal.behaviors.cdMenu={attach:function(context,settings){Drupal.behaviors.cdDropdown.updateDrupalTogglableMenus('.cd-nav .menu button + .menu');}};})(Drupal);;
